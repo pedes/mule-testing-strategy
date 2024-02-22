adb0fbd1387dea2305a8f60033668b43310f0890
Andres Cespedes Morales <andrespedes@gmail.com>
2024-02-22 12:36:46 +0100

    Update test.js

--- src/test/resources/performance/test.js
+++ src/test/resources/performance/test.js
@@ -8,5 +8,5 @@ export const options = {
 };
 
 export default function () {
-  http.get('https://orders-app-enw60f.5sc6y6-1.usa-e2.cloudhub.io/orders');
+  http.get('https://mule-info-adkdbe.5sc6y6-4.usa-e2.cloudhub.io/info');
 }
